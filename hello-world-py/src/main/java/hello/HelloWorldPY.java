da7961e70f3542b292947ca1f392dcf81769fb35
Gabriel Batista <svbgabriel@gmail.com>
2022-06-26 22:15:47 -0300

    Add Python Hello World demo

---
+++ hello-world-py/src/main/java/hello/HelloWorldPY.java
@@ -0,0 +1,19 @@
+package hello;
+
+import org.graalvm.polyglot.*;
+
+public class HelloWorldPY {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java on GraalVM!");
+        try (Context polyglot = Context.newBuilder().build()) {
+            polyglot.eval("python", "print('Hello from Python!');");
+            final Value array = polyglot.eval("python", "[10,22,33,42,49,55]");
+            for (int i = 0; i < array.getArraySize(); i++) {
+                final int number = array.getArrayElement(i).asInt();
+                final String message =
+                    "This number came from a Python array: " + number + "!";
+                System.out.println(message);
+            }
+        }
+    }
+}
