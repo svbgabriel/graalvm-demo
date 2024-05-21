58d6d61469f39a3c753aa3e22950969946f26a56
Gabriel Batista <svbgabriel@gmail.com>
2024-05-20 21:19:00 -0300

    Update math-functions quarkus demo

--- math-functions/src/main/java/io/github/svbgabriel/FunctionService.java
+++ math-functions/src/main/java/io/github/svbgabriel/FunctionService.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.LongStream;
 
-import javax.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class FunctionService {
