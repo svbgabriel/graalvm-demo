4f93165e1a124b9bdf3edf36f77a338c69db7ee2
Gabriel Batista <svbgabriel@gmail.com>
2024-05-22 20:45:07 -0300

    Refactor polyglot-calculator demo code

--- polyglot-calculator/src/main/java/io/github/svbgabriel/Calculator.java
+++ polyglot-calculator/src/main/java/io/github/svbgabriel/Calculator.java
@@ -2,12 +2,10 @@ package io.github.svbgabriel;
 
 public class Calculator {
     public static void main(String[] args) {
-        final Operations operations = new Operations();
-
-        System.out.println(operations.sum(1, 2));
-        System.out.println(operations.sub(9, 2));
-        System.out.println(operations.multi(3, 3));
-        System.out.println(operations.div(4, 2));
-        System.out.println(operations.div(4, 0));
+        System.out.println(Operations.sum(1, 2));
+        System.out.println(Operations.sub(9, 2));
+        System.out.println(Operations.multi(3, 3));
+        System.out.println(Operations.div(4, 2));
+        System.out.println(Operations.div(4, 0));
     }
 }
--- polyglot-calculator/src/main/java/io/github/svbgabriel/Operations.java
+++ polyglot-calculator/src/main/java/io/github/svbgabriel/Operations.java
@@ -7,7 +7,9 @@ import java.util.OptionalInt;
 
 public class Operations {
 
-    private int execute(String operation, int a, int b) {
+    private Operations() {}
+
+    private static int execute(String operation, int a, int b) {
         try (Context polyglot = Context.newBuilder().build()) {
             polyglot.eval("js", Constants.JS_SCRIPT);
 
@@ -19,19 +21,19 @@ public class Operations {
         }
     }
 
-    public int sum(int a, int b) {
+    public static int sum(int a, int b) {
         return execute("sum", a, b);
     }
 
-    public int sub(int a, int b) {
+    public static int sub(int a, int b) {
         return execute("sub", a, b);
     }
 
-    public int multi(int a, int b) {
+    public static int multi(int a, int b) {
         return execute("multi", a, b);
     }
 
-    public OptionalInt div(int a, int b) {
+    public static OptionalInt div(int a, int b) {
         if (b == 0) {
            return OptionalInt.empty();
         }
